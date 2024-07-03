34f87dcd45f692ad9a7646c10b530fdfafbac31a
Gracious Ogyiri Asare <gpoa@bu.edu>
2024-07-03 06:44:36 +0000

    added get request to Geolocation API and user input for location

--- map_app.py
+++ map_app.py
@@ -3,19 +3,27 @@ import json
 import googlemaps
 import os
 import pprint
+import time
+from dotenv import load_dotenv
 
-AUTH_URL = 'https://maps.googleapis.com/maps/'\
-          'api/timezone/json?location=39.6034810%'\
-          '2C-119.6822510&timestamp=1331161200&key=?'\
+load_dotenv()
+API_KEY = os.environ.get('API_KEY')
+location = str(input("Enter city for timezone: ")) #gets user's desired city
+AUTH_URL = f'https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={API_KEY}'
 
-# headers = {
-#   'Content-Type': 'application/json',
-#   'X-Goog-Api-Key': key,
-#   'X-Goog-FieldMask': 'routes.duration,
-#                     routes.distanceMeters,
-#                     routes.polyline.encodedPolyline'
-# }
+response = requests.get(AUTH_URL)
 
-auth_response = requests.get(AUTH_URL)
-# r = requests.get()
-pprint.pprint(auth_response.json())
+lat = response.json()['results'][0]['geometry']['location']['lat']
+long = response.json()['results'][0]['geometry']['location']['lng']
+
+
+time = int(time.time()) #get the current timestamp required for URL:
+
+AUTH_URL2 = 'https://maps.googleapis.com/maps/'\
+          f'api/timezone/json?location={lat}'\
+          f'%2C{long}&timestamp={time}&key={API_KEY}'
+
+
+auth_response = requests.get(AUTH_URL2)
+timezone = auth_response.json()["timeZoneName"]
+print(f"\nThe time zone for {location.title()} is {timezone}") 
