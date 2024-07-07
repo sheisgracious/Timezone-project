d84844c990b014aae2b0e9b89f0afd7af70b2265
Gracious Ogyiri Asare <gpoa@bu.edu>
2024-07-06 19:24:04 -0500

    fixed styling issues

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ API_KEY = os.environ.get('API_KEY')
 
 app = Flask(__name__)
 
+
 def find_timezone(location):
     if not API_KEY:
         return "not found (404 error)"
-    
+
     #get latitude and longitude of user's input
-    geocode_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={API_KEY}'
+    geocode_url = f"https://maps.googleapis.com/maps/api/geocode/"\
+                f"json?address={location}&key={API_KEY}'"
     response = requests.get(geocode_url)
 
     if response.status_code == 200:
@@ -22,15 +24,15 @@ def find_timezone(location):
         lng = response.json()['results'][0]['geometry']['location']['lng']
 
         #get timezone of location
-        timestamp = int(time.time()) #get the current timestamp required for URL:
+        timestamp = int(time.time())  #get the current timestamp required for URL:
         timezone_url = 'https://maps.googleapis.com/maps/'\
-                        f'api/timezone/json?location={lat}'\
-                        f'%2C{lng}&timestamp={timestamp}&key={API_KEY}'
+                    f'api/timezone/json?location={lat}'\
+                    f'%2C{lng}&timestamp={timestamp}&key={API_KEY}'
         time_response = requests.get(timezone_url)
 
         if time_response.status_code == 200:
             timezone = time_response.json()["timeZoneName"]
-            return timezone 
+            return timezone
     return "Invalid input"
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,6 +43,7 @@ def index():
         location = request.form['location']
         timezone = find_timezone(location)
     return render_template('index.html', timezone=timezone, location=location.title())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
